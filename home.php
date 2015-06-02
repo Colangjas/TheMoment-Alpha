af41d7203d1ff4931cfd93493b2ecd37139402b1
Colin Thompson <thompsoncolinm@gmail.com>
2015-06-02 16:33:18 -0400

    continuing development

--- home.php
+++ home.php
@@ -10,13 +10,59 @@
 	// Connect to the server
 	require_once('connectvars.php');
 	
-	// Make sure the user is logged in before going any further.
-	if (!isset($_SESSION['user_id'])) {
-		echo '<p class="login">Please <a href="home.php">log in</a> to access this page.</p>';
-		exit();
-	}
-	
 	// Show the navigation menu
 	require_once('navmenu.php');
+	
+?>	
+
+<p>Enter you username and to sign up to The Moment Alpha.</p>
+		<form method="post" action="<?PHP echo $_SERVER['PHP_SELF']?>">
+			<fieldset>
+				<legend>Registration Info</legend>
+				<label for="username">Username: </label>
+				<input type="text" id="username" name="username" value="<?PHP if (!empty($username)) echo $username; ?>" /><br />
+			</fieldset>
+			<input type="submit" value="Sign Up" name="submit" />
+		</form>
+
+<?PHP
+	
+	// The user already has a log-in and needs to update their current sheet
+	// Grab the user-entered log-in data
+	$user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
+	$user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
+	
+	if (!empty($user_username)) {
+				// Look up the username in the database
+				$query="SELECT user_id, username  FROM users WHERE username = '$user_username'";
+				$data = mysqli_query($dbc, $query);
+				
+				if (mysqli_num_rows($data) == 1) {
+					// The log-in is ok so set the user ID and username session vars (and cookies), and redirect to the home page
+					$row = mysqli_fetch_array($data);
+					$_SESSION['user_id'] = $row['user_id'];
+					$_SESSION['username'] = $row['username'];
+					setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30)); // expires in 30 days
+					setcookie('username', $row['username'], time() + (60 * 60 * 24 * 30)); // expires in 30 days
+					$home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/mindex.php';
+					header('Location: ' . $home_url);
+				}else {
+					// The username/password are incorrect so set an error message
+					$error_msg	= 'You must enter your username and password to log in.';		
+			}
+		}else {
+					// The username/password are incorrect so set an error message
+					$error_msg	= 'You must enter your username and password to log in.';		
+			}
 
 ?>
+
+<p>Enter you username and to login to The Moment Alpha.</p>
+		<form method="post" action="<?PHP echo $_SERVER['PHP_SELF']?>">
+			<fieldset>
+				<legend>Login Info</legend>
+				<label for="username">Username: </label>
+				<input type="text" id="username" name="username" value="<?PHP if (!empty($username)) echo $username; ?>" /><br />
+			</fieldset>
+			<input type="submit" value="Sign Up" name="submit" />
+		</form>
--- navmenu.php
+++ navmenu.php
@@ -3,7 +3,7 @@
 	echo '<hr />';
 	if(isset($_SESSION['username'])) {
 		echo'<a href="home.php">Home</a>';
-		echo'<a href="questionnaire.php">My Results</a>';
+		echo'<a href="yresults.php">My Results</a>';
 		echo'<a href="logout.php">Log Out <span style="color: red;">(' . $_SESSION['username'] . ')</span></a>';
 	} else {
 		echo'Please <a href="login.php">Log In</a> or make a Username.';
