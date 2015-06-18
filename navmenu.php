eb991201ae81891d955c7ed1f645ddc5f55b00f0
Colin Thompson <thompsoncolinm@gmail.com>
2015-06-18 10:59:02 -0400

    creating a side-by-side for register and login

--- navmenu.php
+++ navmenu.php
@@ -1,13 +1,17 @@
 <?PHP
 	// Generate the nanvigation menu
-	echo '<hr />';
+	echo '<nav>';
+	echo '<ul>';
 	if(isset($_SESSION['username'])) {
-		echo'<a class="onblack" href="home.php">Home</a>&nbsp;&nbsp;&nbsp;';
-		echo'<a class="onblack" href="tracker.php">Update Tracker</a>&nbsp;&nbsp;&nbsp;';
-		echo'<a class="onblack" href="yresults.php" target="_blank">My Results</a>&nbsp;&nbsp;&nbsp;';
-		echo'<a class="onblack" href="logout.php">Log Out <span style="color: red;">(' . $_SESSION['username'] . ')</span></a>';
+		echo'<li><a class="onblack" href="home.php">Home</a></li>';
+		echo'<li><a class="onblack" href="tracker.php">Update Tracker</a></li>';
+		echo'<li><a class="onblack" href="yresults.php" target="_blank">My Results</a></li>';
+		echo'<li><a class="onblack" href="logout.php">Log Out <span style="color: red;">(' . $_SESSION['username'] . ')</span></a></li>';
 	} else {
-		echo'Please <a class="onblack" href="home.php">Log In</a> or make a Username.';
+		echo'Please <li>
+			<a class="onblack" href="home.php">Log In</a>
+		</li> or make a Username.';
 	}
-	echo '<hr />';
+	echo '</ul>';
+	echo '</nav>';
 ?>
